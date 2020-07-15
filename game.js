2d5bcbe709f5bcd487b6dc456f80dc442c4a8373
hgsevy <60084574+hgsevy@users.noreply.github.com>
2020-07-15 15:49:24 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,30 @@
+var r, b, g;
+
+function setup(){
+  createCanvas(800, 800);
+  background(163, 129, 83);
+  r = 163;
+  g = 129;
+  b = 83;
+}
+
+function draw(){
+  fill(255);
+  ellipse(590, 400, 50, 50);
+  createImg('https://p7.hiclipart.com/preview/342/1008/947/chameleons-espio-the-chameleon-drawing-coloring-book-black-and-white-figure.jpg', 'Chameleon photo');
+  stroke(0);
+  fill(255);
+  rect(100, 50, 100, 50);
+  textSize(32);
+  text("Change color", 110, 60);
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);
+  if (mouseIsPressed) {
+    if (mouseX > 100 && mouseX < 200 && mouseY > 50 && mouseY < 100) {
+      background(r, g, b);
+    }
+  }
+}
+  
+  
