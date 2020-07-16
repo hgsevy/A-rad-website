20e5fc1fd8f3088c73af9b985f8b601e63d9da99
hgsevy <60084574+hgsevy@users.noreply.github.com>
2020-07-15 17:31:41 -0700

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ function setup(){
   g = 129;
   b = 83;
   fill(255);
-  ellipse(690, 475, 100, 100);
+  ellipse(715, 475, 100, 100);
   image(img, 0, 0);
 }
 
@@ -30,7 +30,7 @@ function draw(){
       background(r, g, b);
       
       fill(255);
-      ellipse(690, 475, 100, 100);
+      ellipse(715, 475, 100, 100);
       image(img, 0, 0);
     }
   }
