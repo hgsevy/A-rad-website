97a5eed54c67cbdecd14cc4859e812364bb82a47
hgsevy <60084574+hgsevy@users.noreply.github.com>
2020-07-16 10:06:27 -0700

    Create pictures.js

---
+++ pictures.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Pictures </title>
+    <script src = "p5.js"> </script>
+		<script src = "img.js"> </script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class = "nav">
+      <h1><a href = "index.html">Lizards</a></h1>
+      <h3><i> They're like snakes with legs </i></h3>
+      <h2 style = "color: green;">
+        <a href = "pictures.html"> Pictures </a>
+        |
+        <a href = "types.html"> Types </a>
+        |
+	      <a href = "facts.html"> Fun Facts </a>
+      </h2>
+		</div>
+    <h1> Pictures </h1>
+    <script id = "script" type = "text/javascript">
+  </body>
+</html>
